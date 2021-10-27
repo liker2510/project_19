1eedc2a0733fb2f0336aeb9cce2eecefd7fc3d8f
liker2510 <81087122+liker2510@users.noreply.github.com>
2021-10-27 14:08:12 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+'use strict';
+const span = document.getElementById('span');
+console.log(span);
+
+let hour = (new Date()).getHours();
+let minutes = (new Date()).getMinutes();
+let seconds =  (new Date()).getSeconds();
+let day = String(new Date());
+let time =new Date()
+
+let dateStop = new Date("December 31, 2021").getTime();
+let dateNow = new Date().getTime();
+let timeRemaining = (dateStop - dateNow) / 1000;
+let hours = Math.floor(timeRemaining / 60 / 60 / 24);
+let days;
+let lastDigit = hours % 10;
+
+if(lastDigit==2||lastDigit==3||lastDigit==4) {
+    days=" дня";
+}else if(lastDigit > 4 && lastDigit < 21) {
+    days=" дней";
+}else if(lastDigit == 1) {
+    days=" день";
+} else {
+    days=" дней";
+}     
+
+if (hour>=5 && hour<12) {
+    span.textContent = `С добрым утром! <br> Сегодня:`;
+} else if (hour>=0 && hour<5) {
+    span.textContent = "Доброй ночи!";
+} else if (hour>=12 && hour<18) {
+    span.innerHTML = `Добрый день! <br> Сегодня: ${day.slice(0, 4)} <br> Текущее время: ${hour} : ${minutes} : ${seconds} <br> До нового года осталось: ${hours} ${days}`;
+} else if (hour>=18 && hour<24) {
+    span.textContent = "Добрый вечер!";
+}
+
+
+
+
+
