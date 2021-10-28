b8af2e851ef6ab9c289865dc55ad2a59a9593d9e
liker2510 <81087122+liker2510@users.noreply.github.com>
2021-10-28 20:29:35 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,40 +2,47 @@
 const span = document.getElementById('span');
 console.log(span);
 
-let hour = (new Date()).getHours();
-let minutes = (new Date()).getMinutes();
-let seconds =  (new Date()).getSeconds();
-let day = String(new Date());
-let time =new Date()
+const getTimeRemaning = () => {
 
-let dateStop = new Date("December 31, 2021").getTime();
-let dateNow = new Date().getTime();
-let timeRemaining = (dateStop - dateNow) / 1000;
-let hours = Math.floor(timeRemaining / 60 / 60 / 24);
-let days;
-let lastDigit = hours % 10;
+    let hour = (new Date()).getHours();
+    let minutes = (new Date()).getMinutes();
+    let seconds = (new Date()).getSeconds();
+    let day = String(new Date());
+    let time = new Date()
 
-if(lastDigit==2||lastDigit==3||lastDigit==4) {
-    days=" дня";
-}else if(lastDigit > 4 && lastDigit < 21) {
-    days=" дней";
-}else if(lastDigit == 1) {
-    days=" день";
-} else {
-    days=" дней";
-}     
+    let dateStop = new Date("December 31, 2021").getTime();
+    let dateNow = new Date().getTime();
+    let timeRemaining = (dateStop - dateNow) / 1000;
+    let hours = Math.floor(timeRemaining / 60 / 60 / 24);
+    let days;
+    let lastDigit = hours % 10;
 
-if (hour>=5 && hour<12) {
-    span.textContent = `С добрым утром! <br> Сегодня:`;
-} else if (hour>=0 && hour<5) {
-    span.textContent = "Доброй ночи!";
-} else if (hour>=12 && hour<18) {
-    span.innerHTML = `Добрый день! <br> Сегодня: ${day.slice(0, 4)} <br> Текущее время: ${hour} : ${minutes} : ${seconds} <br> До нового года осталось: ${hours} ${days}`;
-} else if (hour>=18 && hour<24) {
-    span.textContent = "Добрый вечер!";
+
+    if (lastDigit == 2 || lastDigit == 3 || lastDigit == 4) {
+        days = " дня";
+    } else if (lastDigit > 4 && lastDigit < 21) {
+        days = " дней";
+    } else if (lastDigit == 1) {
+        days = " день";
+    } else {
+        days = " дней";
+    }
+
+    if (hour >= 5 && hour < 12) {
+        span.innerHTML = `С добрым утром! <br> Сегодня: ${day.slice(0, 4)} <br> Текущее время: ${hour} : ${minutes} : ${seconds} <br> До нового года осталось: ${hours} ${days}`;
+    } else if (hour >= 0 && hour < 5) {
+        span.innerHTML = `Доброй ночи! <br> Сегодня: ${day.slice(0, 4)} <br> Текущее время: ${hour} : ${minutes} : ${seconds} <br> До нового года осталось: ${hours} ${days}`;
+    } else if (hour >= 12 && hour < 18) {
+        span.innerHTML = `Добрый день! <br> Сегодня: ${day.slice(0, 4)} <br> Текущее время: ${hour} : ${minutes} : ${seconds} <br> До нового года осталось: ${hours} ${days}`;
+    } else if (hour >= 18 && hour < 24) {
+        span.innerHTML = `Добрый вечер! <br> Сегодня: ${day.slice(0, 4)} <br> Текущее время: ${hour} : ${minutes} : ${seconds} <br> До нового года осталось: ${hours} ${days}`;
+    }
+
 }
 
-
+let idInterval = setInterval(() => {
+    getTimeRemaning()
+}, 1000);
 
 
 
